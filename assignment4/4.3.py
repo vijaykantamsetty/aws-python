a0087df873f1c3501e53ec216d8a6b48c56885fc
vijaykantamsetty <147807986+vijaykantamsetty@users.noreply.github.com>
2024-08-02 21:13:20 +0530

    Add files via upload

---
+++ assignment4/4.3.py
@@ -0,0 +1,13 @@
+name = "some name"
+
+upper_name = name.upper()
+print(upper_name)  # "SOME NAME"
+
+lower_name = name.lower()
+print(lower_name)  # "some name"
+
+capitalized_name = name.capitalize()
+print(capitalized_name)  # "Some name"
+
+replaced_name = name.replace('e', 'E')
+print(replaced_name)  # "somE namE"
